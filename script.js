3a071cdcbe6118d3d8c112c409864ccea44eb7b8
mohdhanif1234 <88095538+mohdhanif1234@users.noreply.github.com>
2023-05-25 20:29:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function highlight() {
     //Write your code here
-
-
+	var boldElements = document.querySelectorAll("strong");
+    for (var i = 0; i < boldElements.length; i++) {
+    boldElements[i].style.color = "green";
+	}
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+	var boldElements = document.querySelectorAll("strong");
+    for (var i = 0; i < boldElements.length; i++) {
+    boldElements[i].style.color = "black";
+	}
 }
